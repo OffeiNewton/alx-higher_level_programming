969840e6b07d3cc22dad3c9a824abad65f803dca
offeiNewton <solomonwright9@gmail.com>
2023-06-07 22:30:07 -0700

    new update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# ASCII values for lowercase letters start from 97
-# Loop through the ASCII range and print the corresponding character using string format
-for i in range(97, 123):
-    print("{:c}".format(i), end="")
+# ASCII value for lowercase 'a' is 97
+# Loop through the range of 26 (number of lowercase letters) and print the corresponding character using string format
+for i in range(26):
+    print(chr(97 + i), end="")
