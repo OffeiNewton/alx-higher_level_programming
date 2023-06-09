e3ca4e1b932f6da53a56fdb31bce794c10f2469b
offeiNewton <solomonwright9@gmail.com>
2023-06-08 22:39:03 -0700

    final update

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
-import string
+def magic_calculation(a, b):
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
 
-print(string.ascii_uppercase)
-
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
